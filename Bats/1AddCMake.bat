79fa7cd4d68f810cec5c22d6e2c1e83c36405e2f
filinouns <andpuent@ucm.es>
2020-02-18 10:00:05 +0100

    Aclaracion bat1

--- Bats/1AddCMake.bat
+++ Bats/1AddCMake.bat
@@ -15,6 +15,7 @@ MD ..\ProyectoBasico\Dependencias\ogre-1.12.4\Build
 :: Poned la ubicacion del exe en el proyecto
 echo ---------------------------------------------------
 echo ejecucion del exe de CMake
+echo Si os da problema al hacer el "generate" -> buscar "imgui" desmarcarlo y volver a generar
 echo ---------------------------------------------------
 start ..\ProyectoBasico\Dependencias\cmake\bin\cmake-gui.exe
 
