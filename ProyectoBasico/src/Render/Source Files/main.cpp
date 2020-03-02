8a6437f83a886a886bf9508069418dd9370af1de
Alvaro Serna <alsern01@ucm.es>
2020-03-03 00:38:48 +0100

    renderizado de ventana hecho
    falta solucionar errores de link

--- ProyectoBasico/src/Render/Source Files/main.cpp
+++ ProyectoBasico/src/Render/Source Files/main.cpp
@@ -6,36 +6,12 @@
 //#include <OgreRenderWindow.h>
 
 //Podemos llamar a Ogre.h directamente e incluiriamos estas y mas
-#include <Ogre.h>
-#include "OgreApp.h"
 #include "WindowRenderer.h"
-#include <Application2.h>
 #include <SDL.h>
 
 
-#ifdef _DEBUG
 int main()
-#else
-#include <Windows.h>
-int WINAPI
-WinMain(HINSTANCE hinstance, HINSTANCE prevInstance, LPSTR lpCmdLine, int nCmdShow)
-#endif
 {
-	/*
-	try	{
-		OgreEasy::OgreApp OgreApp;
-		OgreApp.AnOgreApplication();
-		std::cout << "end of the program" << std::endl;
-	}
-	catch (Ogre::Exception & e)	{
-		MWARNING("!!!!Ogre::Exception!!!!\n" << e.what());
-	}
-	catch (std::exception & e) {
-		MWARNING("!!!!std::exception!!!!\n" << e.what());
-	}
-
-	OgreEasy::waitForUser();
-	*/
 	WindowRenderer* renderManager= WindowRenderer::getSingleton();
 	
 	bool exit_ = false;
