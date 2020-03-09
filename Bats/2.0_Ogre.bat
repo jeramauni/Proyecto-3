24834d31c5a23faf110c2ea1573286e1e23e7b7e
Filinouns <andpuent@ucm.es>
2020-03-09 17:22:04 +0100

    --...--

---
+++ Bats/2.0_Ogre.bat
@@ -0,0 +1,9 @@
+@echo off
+
+echo ---------------------------------------------
+echo Iniciando proyecto de Ogre generado en build. Compilar en debug x64 y release x64
+echo ---------------------------------------------
+
+start ..\ProyectoBasico\Dependencias\ogre-1.12.5\Build\OGRE.sln
+
+PAUSE
---
+++ Bats/2.1_OIS.bat
@@ -0,0 +1,12 @@
+@echo off
+
+
+
+
+echo ---------------------------------------------
+echo Iniciando proyecto de OIS generado en build. Compilar en debug x64 y release x64
+echo ---------------------------------------------
+
+start ..\ProyectoBasico\Dependencias\OIS\Build\OIS.sln
+
+PAUSE
