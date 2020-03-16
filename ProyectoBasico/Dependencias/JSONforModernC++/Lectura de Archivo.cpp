2fb0f5e54bf08ad58345ac872d878cd21f1b150f
DavidCarmonaFauste <davicarm@ucm.es>
2020-03-16 20:18:18 +0100

    componentes por json, lectura y parseado

--- ProyectoBasico/Dependencias/JSONforModernC++/Lectura de Archivo.cpp
+++ ProyectoBasico/Dependencias/JSONforModernC++/Lectura de Archivo.cpp
@@ -1,61 +1,53 @@
-#include "../jsonv350.hpp"
-using namespace std;
-// Lectura de Archivo.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-#include <vector>
 #include <fstream>
 using json = nlohmann::json;
 #include <iostream>
 
 int main()
 {
-	fstream stream("entities.json");
-
-	struct Componente
-	{
-		string id;
-	};
-
-	struct transform:Componente
-	{
-		int posicion[3];
-		int escala[3];
-		int rotacion[3];
-	};
-
-	struct physic :Componente
-	{
-		bool collision;
-		int escala[3];
-	};
+	json entitiesFile;
+	json mapFile;
 
 
-	struct Entity
-	{
-		string id;
-		vector<Componente*> c;
+	streamE >> entitiesFile;
+	streamM >> mapFile;
 
-	};
+	//file.dump(stream);
+	entitiesFile = entitiesFile.at("Entities");
+	mapFile = mapFile.at("Map");
 
+	int sizeE = entitiesFile.size();
+	int sizeM = mapFile.size();
+	for (int i = 0; i < sizeM; i++) {		// recorre el mapa de entidades
+		string id = mapFile[i].at["id"].get<string>();
+		string nombre = mapFile[i].at["nombre"].get<string>();
 
-	json file;
-	stream >> file;
-	file = file.at("Entities");
+		json entidad = entitiesFile[id];
+		json components = mapFile[i].at["components"];
 
-	int size = file.size();
-
-	for(int i = 0; i < size; i++){
-		Entity e;
-		e.id = file[i].at["id"].get<string>();
-		for (int j = 0; j < size; j++) {
-			string tipo = (file[i].at["components"][j].at["id"].get<string>());
-			if ("physic") physic c;
-			break;
-			}
-			e.c.push_back(comp);
-
+		for (int j = 0; j < components.size(); j++) {		//recorre los componentes
+			string tipo = components[j].at["id"];
+			entidad[tipo] = components[j];
+			//crear un componente y pasarle los parámetros tipo json
 		}
+
+		entidad["nombre"] = nombre;
+
+		//entidad ya tiene los componentes actualizados y un nuevo campo para el nombre
+
+		
+
 	}
+
+
+	//for(int i = 0; i < size; i++){
+	//	Entity e;
+	//	//e.id = file[i].at["id"].get<string>();
+	//	for (int j = 0; j < size; j++) {
+	//		//string tipo = (file[i].at["components"][j].at["id"].get<string>());
+	//		
+	//		//e.c.push_back(comp);
+
+	//	}
+	//}
 	//cout << j["Entities"]["id"];
 }
-
